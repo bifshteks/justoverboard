95b2920e4505d4af06d7e8508e7b8468977c4431
Artem <artem.pif@mail.ru>
2018-02-27 19:44:15 +0300

    seems like it can work on heroku

--- server.js
+++ server.js
@@ -116,3 +116,7 @@
     // listen (start app with node server.js) ======================================
     app.listen(8080);
     console.log("App listening on port 8080");
+
+
+
+    module.exports = app;
