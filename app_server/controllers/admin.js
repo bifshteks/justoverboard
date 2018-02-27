2a2d8f375a284bd172bab12177d9c8cc0ec55f75
Artem <artem.pif@mail.ru>
2018-02-27 19:29:09 +0300

    cleaning and reborn

--- app_server/controllers/admin.js
+++
@@ -1,3 +0,0 @@
-module.exports.indexAdmin = function(req, res){
-  res.render('admin', { title: 'Админка' });
-};
--- app_server/controllers/cart.js
+++
@@ -1,3 +0,0 @@
-module.exports.cart = function(req, res){
-  res.render('cart', { title: 'Корзина' });
-};
